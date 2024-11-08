67ad274fd71eebb9a5a6d305e490e8c8ded3a183
marambrahmi <marambrahmi60@gmail.com>
2024-11-08 11:28:49 +0100

    first step

---
+++ src/test/java/pi/pperformance/elite/EliteApplicationTests.java
@@ -0,0 +1,13 @@
+package pi.pperformance.elite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
